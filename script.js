5cdced4ed533d3b71609b4463a34facac599dd8a
Manan357 <padaliya828@gmail.com>
2024-11-20 16:03:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Birthday Cake Section: Blow out the candle
+document.getElementById('blow-candle-btn').addEventListener('click', function() {
+  document.getElementById('wish-text').textContent = "The candle is blown out! Make a wish!";
+});
